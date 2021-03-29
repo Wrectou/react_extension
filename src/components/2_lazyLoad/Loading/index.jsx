38db8e97b897f6ada930f7b9d2204fc9a835aa8a
wrectou <wrectou@gmail.com>
2021-03-29 22:48:10 +0800

    2_lazyLoad

---
+++ src/components/2_lazyLoad/Loading/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Loading extends Component {
+  render() {
+    return (
+      <div>
+        <h2 style={{backgroundColor: '#f1f1f1', color: '#333'}}>Loading...</h2>
+      </div>
+    )
+  }
+}
