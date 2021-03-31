0b37963ca3e1493ba0ba9ae08fc51395cad53479
wrectou <wrectou@gmail.com>
2021-03-31 11:15:40 +0800

    4_fragment

---
+++ src/components/4_fragment/index.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+import ReactDom from 'react-dom'
+
+class Demo extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <input type="text"/>
+        <input type="text"/>
+      </Fragment>
+    )
+  }
+}
+
+export default Demo
